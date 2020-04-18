521fbb8158d7b5445a4a07cc3612a1f328ffe128
ym001 <yves.mercadier@gmail.com>
2020-04-18 18:04:48 +0200

    test

---
+++ Manteia/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  __init__.py
+#  
+#  Copyright 2020 Yves <yves@mercadier>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+"""
+    Ce module proclame la bonne parole de sieurs Sam et Max. Puissent-t-ils
+    retrouver une totale liberté de pensée cosmique vers un nouvel age
+    reminiscent.
+"""
+ 
+__version__ = "0.0.1"
+
+from Manteia.Manteia import testManteia
+
---
+++ README.md
@@ -0,0 +1,17 @@
+Manteia - Proclame la bonne parole 
+================================
+
+Ce module proclame la bonne parole. Puissent-t-ils
+retrouver une totale liberté de pensée cosmique vers un nouvel age
+reminiscent.
+
+Vous pouvez l'installer avec pip:
+
+    pip install Manteia
+
+Exemple d'usage:
+
+    >>> from Manteia import testManteia
+    >>> testManteia()
+
+Ce code est sous licence WTFPL.

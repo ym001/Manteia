235648ac21543f0294b31cdfdf4622b0a241bd2b
ym001 <yves.mercadier@gmail.com>
2020-05-08 12:24:25 +0200

    lk

--- docs/conf.py
+++ docs/conf.py
@@ -49,15 +49,24 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-##html_theme = 'classic'
-import sphinx_glpi_theme
-html_theme = "glpi"
-html_theme_path = sphinx_glpi_theme.get_html_themes_path()
+import os
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = ["_themes", ]
+'''
+if on_rtd:
+	
+	html_theme = 'classic'
+	# Add any paths that contain custom static files (such as style sheets) here,
+	# relative to this directory. They are copied after the builtin static files,
+	# so a file named "default.css" will overwrite the builtin "default.css".
+	html_static_path = ['_static']
+else:
+	import sphinx_glpi_theme
+	html_theme = "glpi"
+	html_theme_path = sphinx_glpi_theme.get_html_themes_path()
+'''
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
 
 ####################
 #rajoute perso
